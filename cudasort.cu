e7d814724f42f7c5d6e7393105b8a66d2834b80c
Parth Nagori <nagori.parth@gmail.com>
2019-04-15 19:03:41 -0400

    mc

--- cudasort.cu
+++ cudasort.cu
@@ -1,114 +1,73 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <assert.h>
-#include <sys/time.h>
+ #ifdef __cplusplus
+  extern "C"
+  {
+  #endif
 
-#define THREADS 512
-#ifdef __cplusplus
-extern "C"
+
+__global__ void bitonic_sort(float *gpu_val, int j, int k)
 {
-#endif
+  unsigned int i, ij; 
+    i = threadIdx.x + blockDim.x * blockIdx.x;
+  ij = i^j;
 
-__device__ void merge(float* arr, float* final, int start, int mid, int end)
-{
-    int i = start;
-    int j = mid;
-    int k = start;
-    printf("\nstart : %d mid: %d end: %d", start, mid, end);
-    while (k < end)
-    {
-      if (i==mid){
-        final[k] = arr[j++];
+  if ((ij)>i) {
+    if ((i&k)==0) {
+      if (gpu_val[i]>gpu_val[ij]) {
+        float temp = gpu_val[i];
+        gpu_val[i] = gpu_val[ij];
+        gpu_val[ij] = temp;
       }
-      else if (j == end){
-        final[k] = arr[i++];
-      }
-      else if (arr[i] < arr[j]){
-        final[k] = arr[i++];
-      }
-      else{
-        final[k] = arr[j++];
-      }
-      k++;
     }
-
-    for(i = start; i < end; i++)
-    {
-      arr[i] = final[i];
+    if ((i&k)!=0) {
+      if (gpu_val[i]<gpu_val[ij]) {
+        float temp = gpu_val[i];
+        gpu_val[i] = gpu_val[ij];
+        gpu_val[ij] = temp;
+      }
     }
-
+  }
 }
 
-__global__ void merge_sort(float* arr, float* final, int numberOfBlocks, int elementsPerBlock, int partition){
-
-    int block_id = blockIdx.x;
-    printf("\nblock id : %d partition: %d", block_id, partition);
-    int start = block_id * partition;
-    if (start >= 0)
-    {
-      int n = numberOfBlocks*elementsPerBlock;
-      int end = min(start + partition,n);
-      int mid = min(start + partition/2,n);
-      merge(arr, final, start, mid, end);
-    }
-}
 
 int cuda_sort(int number_of_elements, float *a)
 {
   
   float *arr;
-  float *final;
-  // int n;
-  // int part = 0;
-
-  int numberOfBlocks = 512;
-  int elementsPerBlock = number_of_elements/numberOfBlocks;
-
-  cudaEvent_t event;
-  cudaEventCreate(&event);
-
+  
   cudaMalloc((void **) &arr, sizeof(float)*number_of_elements);
-  cudaMalloc((void **) &final, sizeof(float)*number_of_elements);
   cudaMemcpy(arr, a, sizeof(float)*number_of_elements, cudaMemcpyHostToDevice);
 
-  dim3 dimGrid(numberOfBlocks);
-  dim3 dimBlock(1);
+  int threads_create = 512;
+  int blocks_create = number_of_elements/512;
+  //  if(number_of_elements % 512 == 0)
+  // {
+  //   threads_create = 512;
+  //   blocks_create = number_of_elements/512;
+  // }
+  //   else if(number_of_elements < 512){
+  //   threads_create =number_of_elements;
+  //   blocks_create = 1;
+  // }
+  //   else{
+  // threads_create = number_of_elements%512;
+  // blocks_create = number_of_elements/512;
+  // }
+  dim3 blocks(blocks_create);    /* Number of blocks   */
+  dim3 threads(threads_create);  /* Number of threads  */
 
-  int partition;
-  // int partitions;
-
-  int cnt = 0;
-  // n = number_of_elements;
-  // while (n != 0){
-  //   ++partitions;
-  //   n/=2;
-  // } 
-
-
-  // for (part = 0; part < partitions; part++){
-  //   int part_size = part << 1;
-  //   merge_sort<<<dimGrid, dimBlock>>>(arr, final, numberOfBlocks, elementsPerBlock, part); 
-  // } 
-
-
-  for (partition = 2; partition < 2*number_of_elements; partition*=2) {
-    // if (cnt%2 == 0)
-    merge_sort<<<1, 1>>>(arr, final, numberOfBlocks, elementsPerBlock, partition); 
-    // else
-    //   merge_sort<<<dimGrid, dimBlock>>>(final, arr, numberOfBlocks, elementsPerBlock, partition);
-    cnt+=1; 
+  int l, m;
+  for (l = 2; l <= number_of_elements; l <<= 1) {
+    for (m=l>>1; m>0; m=m>>1) {
+      bitonic_sort<<<blocks, threads>>>(arr, m, l);
+    }
   }
+  cudaMemcpy(a, arr, sizeof(float)*number_of_elements, cudaMemcpyHostToDevice);
+  cudaFree(arr);
 
-  printf("\ncnt: %d", cnt);
-  cudaMemcpy(a, final, sizeof(float)*number_of_elements, cudaMemcpyDeviceToHost);
-  // cudaFree(gpu_arr);
-  cudaThreadSynchronize();
-  cudaEventSynchronize(event);
-  
   return 0;
 }
 
 #ifdef __cplusplus
 }
 #endif
+
