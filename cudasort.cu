61510cf08c818551ccf741817398e7a1e63059b2
Parth Nagori <nagori.parth@gmail.com>
2019-04-15 19:17:02 -0400

    mc

--- cudasort.cu
+++ cudasort.cu
@@ -1,7 +1,14 @@
- #ifdef __cplusplus
-  extern "C"
-  {
-  #endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <sys/time.h>
+
+#define THREADS 512
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
 
 __global__ void bitonic_sort_step(float *arr, int j, int k)
@@ -33,33 +40,32 @@ int cuda_sort(int number_of_elements, float *a)
 {
   
   float *arr;
-  size_t size = number_of_elements * sizeof(float);
-
+  
   cudaMalloc((void**) &arr, number_of_elements * sizeof(float));
   cudaMemcpy(arr, a, number_of_elements * sizeof(float), cudaMemcpyHostToDevice);
   
-  int threads_create = 0;
-  int blocks_create = 0;
+  int thread_cnt = 0;
+  int block_cnt = 0;
    if(number_of_elements % 512 == 0)
   {
-    threads_create = 512;
-    blocks_create = number_of_elements/512;
+    thread_cnt = 512;
+    block_cnt = number_of_elements/512;
   }
-    else if(number_of_elements < 512){
-    threads_create =number_of_elements;
-    blocks_create = 1;
-  }
-    else{
-  threads_create = number_of_elements%512;
-  blocks_create = number_of_elements/512;
-  }
-  dim3 blocks(blocks_create,1);    /* Number of blocks   */
-  dim3 threads(threads_create,1);  /* Number of threads  */
+  //   else if(number_of_elements < 512){
+  //   thread_cnt =number_of_elements;
+  //   block_cnt = 1;
+  // }
+  //   else{
+  // thread_cnt = number_of_elements%512;
+  // block_cnt = number_of_elements/512;
+  // }
+  dim3 blocks_per_grid(block_cnt,1);    /* Number of blocks   */
+  dim3 threads_per_block(thread_cnt,1);  /* Number of threads  */
 
   int l, m;
   for (l = 2; l <= number_of_elements; l <<= 1) {
     for (m=l>>1; m>0; m=m>>1) {
-      bitonic_sort_step<<<blocks, threads>>>(arr, m, l);
+      bitonic_sort_step<<<blocks_per_grid, threads_per_block>>>(arr, m, l);
     }
   }
   cudaMemcpy(a, arr, number_of_elements * sizeof(float), cudaMemcpyDeviceToHost);
